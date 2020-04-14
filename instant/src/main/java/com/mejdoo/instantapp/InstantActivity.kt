2fb4ad09fbeaf9c843c8bfe2011d50d6c368176a
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2020-04-14 12:04:38 +0200

    refactor: move instant app code to 'instant' module and rename 'app' module to 'installed'

--- instant/src/main/java/com/mejdoo/instantapp/InstantActivity.kt
+++ instant/src/main/java/com/mejdoo/instantapp/InstantActivity.kt
@@ -1,9 +1,11 @@
 package com.mejdoo.instantapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.instantapps.InstantApps
 import kotlinx.android.synthetic.main.activity_instant.*
 
 class InstantActivity : AppCompatActivity() {
@@ -21,8 +23,24 @@ class InstantActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.action_download -> true
+            R.id.action_download -> {
+                showInstallPrompt()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun showInstallPrompt() {
+        val postInstall = Intent(Intent.ACTION_MAIN)
+            .addCategory(Intent.CATEGORY_DEFAULT)
+            .setPackage(BuildConfig.APPLICATION_ID)
+
+        // The request code is passed to startActivityForResult().
+        InstantApps.showInstallPrompt(this, postInstall, REQUEST_CODE, /* referrer= */ null)
+    }
+
+    companion object {
+        const val REQUEST_CODE = 1
     }
 }
